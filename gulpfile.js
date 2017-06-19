aef04b53f179a592fa62ecbe14ced77c46661f22
Yana Slobodianyk <uxbeginner1989@gmail.com>
2017-06-19 13:38:12 +0300

    create template

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var gulp = require('gulp');
+
+// 1 task: compile less to scss
+//lessc src/css/less/bootstrap-custom.less src/css/main.css
+// find example of task which can run comand in the console
+
+
