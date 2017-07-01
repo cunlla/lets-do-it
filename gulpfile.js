c4e2464af53f7c1a7734c9c23dfe37f0075a3273
Yana Slobodianyk <uxbeginner1989@gmail.com>
2017-07-01 18:57:38 +0300

    Add main page, and its styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 "use strict";
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	browserSync = require('browser-Sync').create();
+
 
 // 1 task: compile less to scss
 //lessc src/css/less/bootstrap-custom.less src/css/main.css
 // find example of task which can run comand in the console
+gulp.task('serve', function(){
+    browserSync.init({
+        server: 'public'
+    });
 
+        
+});
 
